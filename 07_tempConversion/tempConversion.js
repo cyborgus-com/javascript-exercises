b98a9e2487cad62b7a607d34f6b8bdf2ef1bce74
karolisurbonas <karolis.urbonas@gmail.com>
2024-11-13 14:57:27 -1000

    07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5/9);
+  if (Math.floor(tempC)==0) return tempC;
+  else return tempC.toFixed(1)*1.0;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * 9/5) + 32;
+  if (Math.floor(tempF)==0) return tempF;
+  else return tempF.toFixed(1)*1.0;
 };
 
 // Do not edit below this line
